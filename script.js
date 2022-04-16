73c988a4d010f77d63737ab570e697253277257c
Petr Pouč <62241602+surViVeCZ@users.noreply.github.com>
2022-04-16 21:07:59 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+  
+  window.addEventListener("scroll", reveal);
+
+  //visible navbar
+  window.addEventListener("scroll", function(){
+      var header = document.querySelector("nav");
+      header.classList.toggle("sticky", window.scrollY > 600);
+
+  })
+    //active section
+    const sections = document.querySelectorAll("section");
+    const navLi = document.querySelectorAll("#navs li");
+    window.addEventListener("scroll", () => {
+    let current = "";
+
+
+    sections.forEach((section) => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.clientHeight;
+        if (pageYOffset >= sectionTop - sectionHeight / 3) {
+        current = section.getAttribute("id");
+        }
+        console.log(current)
+    });
+
+    navLi.forEach((li) => {
+        li.classList.remove("active");
+        if (li.classList.contains(current)) {
+        li.classList.add("active");
+        }
+    });
+    });
+
