c52d1e7e41f0d255a26a1ede89cdd4d9d3bc8481
Petr Pouč <62241602+surViVeCZ@users.noreply.github.com>
2022-04-18 22:03:32 +0200

    contacts

--- script.js
+++ script.js
@@ -46,3 +46,21 @@ function reveal() {
     });
     });
 
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function next_prev(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  x[slideIndex-1].style.display = "block";  
+}
+
