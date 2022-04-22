91df0f6fbe372d16d7e13a67fa04a400fa55465d
surViVeCZ <poucpetr11@seznam.cz>
2022-04-22 20:20:09 +0200

    another fucking stupid nav

--- script.js
+++ script.js
@@ -1,66 +1,68 @@
 function reveal() {
-    var reveals = document.querySelectorAll(".reveal");
-  
-    for (var i = 0; i < reveals.length; i++) {
-      var windowHeight = window.innerHeight;
-      var elementTop = reveals[i].getBoundingClientRect().top;
-      var elementVisible = 150;
-  
-      if (elementTop < windowHeight - elementVisible) {
-        reveals[i].classList.add("active");
+    const reveal_item = document.querySelectorAll(".reveal");
+    for (var i = 0; i < reveal_item.length; i++) {
+      var height = window.innerHeight;
+      var elementTop = reveal_item[i].getBoundingClientRect().top;
+    
+      //with less value than 100, you can reveal objects faster
+      if (elementTop < height - 100) {
+        reveal_item[i].classList.add("active");
       } else {
-        reveals[i].classList.remove("active");
+        reveal_item[i].classList.remove("active");
       }
     }
   }
   
   window.addEventListener("scroll", reveal);
 
-  //visible navbar
-  window.addEventListener("scroll", function(){
-      var header = document.querySelector("#header-menu");
-      header.classList.toggle("sticky", window.scrollY > 600);
+//always visible navbar
+window.addEventListener("scroll", function(){
+    var header = document.querySelector("#header-menu");
+    header.classList.toggle("sticky", window.scrollY > 600);
 
-  })
-    //active section
-    const sections = document.querySelectorAll("section");
-    const navLi = document.querySelectorAll("#navs li");
-    window.addEventListener("scroll", () => {
-    let current = "";
+})
+  //active section in navbar
+  const sections = document.querySelectorAll("section");
+  const navLi = document.querySelectorAll("#navs li");
+  window.addEventListener("scroll", () => {
+  let current = "";
 
 
-    sections.forEach((section) => {
-        const sectionTop = section.offsetTop;
-        const sectionHeight = section.clientHeight;
-        if (pageYOffset >= sectionTop - sectionHeight / 3) {
+  sections.forEach((section) => {
+      const top = section.offsetTop;
+      const height = section.clientHeight;
+      if (pageYOffset >= top - height / 3) {
         current = section.getAttribute("id");
-        }
-        console.log(current)
-    });
+      }
+      console.log(current)
+  });
 
-    navLi.forEach((li) => {
-        li.classList.remove("active");
-        if (li.classList.contains(current)) {
-        li.classList.add("active");
-        }
+  navLi.forEach((li) => {
+      li.classList.remove("active");
+      if (li.classList.contains(current)) {
+      li.classList.add("active");
+      }
     });
-    });
+  });
 
-var slideIndex = 1;
-showDivs(slideIndex);
 
 function next_prev(n) {
-  showDivs(slideIndex += n);
+  picture_show(index += n);
 }
-
-function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length}
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
+var index = 1;
+picture_show(index);
+    
+//img carousel in achievements section
+function picture_show(n) {
+  const x = document.getElementsByClassName("show");
+  var l;
+  if (n > x.length) {index = 1}
+  if (n < 1) {
+    index = x.length
   }
-  x[slideIndex-1].style.display = "block";  
+  for (l = 0; l < x.length; l++) {
+    x[l].style.display = "none";  
+  }
+  x[index-1].style.display = "block";  
 }
 
